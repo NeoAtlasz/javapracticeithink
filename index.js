b50c8c1aca1fa854c98e83b72317095996118e7c
NeoAtlasz <y0shii103288@gmail.com>
2025-06-17 03:59:29 +0000

    javaPart2

--- index.js
+++ index.js
@@ -2,3 +2,22 @@
  console.log('Hello World');
  let name = 'Neo';
  console.log(name);
+
+ // Cannot be a reserved keyword
+ // Should be meaningful
+ // Cannot start with a number (1name)
+ // Cannot contain space or hyphen (-)
+ // Are case-sensitive 
+
+
+ let firstName = 'Neo'; // String Literal
+ let lastName = 'Atlas';
+ let age = 18; //Number Literal
+ let isApproved = false; //Boolean Literal
+ let firstName; undefined; 
+ let selectedColor = null;
+
+ const interestRate = 0.3;
+ interestRate = 0.3;
+ console.log(interestRate); 
+ 
